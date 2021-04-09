a92bdd305d64455b6dfb1b7ed45ba58f1076dced
neelbagora <neelbagora@gmail.com>
2021-04-08 21:49:58 -0400

    Restructure files

---
+++ src/data_load.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# For Documentation Purposes
+'''
+    Cleans raw data by removing unnecessary features,
+    writing new data frame into 'weather_data.csv', and compressing
+    into 'out.zip'
+'''
+data = pd.read_csv("weatherAUSData.csv")
+del data['Date']
+del data['Location']
+del data['Evaporation']
+del data['Sunshine']
+compression_opts = dict(method='zip', archive_name='weather_data.csv')
+data.to_csv('out.zip', index=False, compression=compression_opts)
