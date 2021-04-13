67b3cefe52d66caf5677a9df4047680b22676e88
neelbagora <neelbagora@gmail.com>
2021-04-12 20:28:49 -0400

    Renamed dtc parameters to X and y

--- src/decision_tree_classifier.py
+++ src/decision_tree_classifier.py
@@ -4,10 +4,10 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score
 
-# Input: Pandas Dataframe input_data X
+# Input: Pandas Dataframe X
 #           dataframe containing the data to be used to form the decision
 #           tree model
-#        Pandas Dataframe outputs y
+#        Pandas Dataframe y
 #           dataframe containing the outputs corresponding to the input_data
 #           provided.
 #        float impurity_decrease
@@ -19,27 +19,27 @@ from sklearn.metrics import accuracy_score
 # Output: float accuracy_score of the validation subset
 #         numpy vector y-hat (using testing data), of size 200
 #         numpy vector y (using testing data), of size 200
-def decisiontreeclassifier(input_data, outputs, impurity_decrease, np_seed=None):
+def decisiontreeclassifier(X, y, impurity_decrease, np_seed=None):
     if np_seed:
         np.random.seed(np_seed)
 
     # randomly select indices to be used for training, validation, and testing
-    testing_indices = list(np.random.choice(len(input_data), len(input_data), replace=False))
+    testing_indices = list(np.random.choice(len(X), len(X), replace=False))
 
     # training subset will utilize 60% of the data
-    training_data = input_data.iloc[testing_indices[0 : 600]]
-    training_data_output = outputs.iloc[testing_indices[0 : 600]]
+    training_data = X.iloc[testing_indices[0 : 600]]
+    training_data_output = y.iloc[testing_indices[0 : 600]]
 
     # validation subset will utilize 20% of the remaining data
-    validation_data = input_data.iloc[testing_indices[600 : 800]]
-    validation_data_output = outputs.iloc[testing_indices[600 : 800]]
+    validation_data = X.iloc[testing_indices[600 : 800]]
+    validation_data_output = y.iloc[testing_indices[600 : 800]]
 
     # testing subset will utilize the remaining 20% of data
-    testing_data = input_data.iloc[testing_indices[800 : 1000]]
+    testing_data = X.iloc[testing_indices[800 : 1000]]
     # print(testing_data)
 
     clf = DecisionTreeClassifier(min_impurity_decrease=impurity_decrease)
     clf = clf.fit(training_data, training_data_output)
     clf_testing_predict = clf.predict(testing_data)
-    testing_output = np.array(outputs.iloc[testing_indices[800:]])
+    testing_output = np.array(y.iloc[testing_indices[800:]])
     return (accuracy_score(clf.predict(validation_data), validation_data_output), clf_testing_predict, testing_output)
