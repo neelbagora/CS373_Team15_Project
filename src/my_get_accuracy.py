dbac9c922964965f97483f2a4e614f762138128e
Aaron Sun <aaronsun@live.com>
2021-04-22 21:08:57 -0700

    Implemented my own accuracy calculator

---
+++ src/my_get_accuracy.py
@@ -0,0 +1,12 @@
+def run(X, y, normalize):
+    match = 0
+    total = len(X)
+
+    for i in range(0, total):
+        if X[i] == y[i]:
+            match += 1
+
+    if normalize:
+        return 1.0 * match / total
+
+    return match
